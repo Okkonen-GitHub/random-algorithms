c6d0c6452342f225b72d06da1086b8795bf2464c
Okko <okko.contact@proton.me>
2022-09-29 19:17:55 +0300

    comment code

--- splitmethod.py
+++ splitmethod.py
@@ -1,9 +1,10 @@
 
 from random import randint
 
+# x-koordinaatti läheltä funktion nollakohtaa
 N = 10
 
-
+# esittää funktiota, jonka nollakohta halutaan ratkaista
 def f(x):
     a=-2.8
     b=1.7
@@ -11,31 +12,35 @@ def f(x):
     d=3.3
     return a*(x**3) + b*(x**2) + c*x + d
 
+
 def solve(f, a: int, b: int) -> int:
 
+    # laskee kahden pisteen puolivälissä olevan pisteen x-koordinaatin
     def middlepoint(x1, x2):
         return (x1 + x2)/2
 
+    # arvot f(a) ja f(b)
     a_f, b_f = f(a), f(b)
 
     precision = 10
 
+    # f(a) ja f(b) ovat erimerkkiset
     if (a_f > 0 and b_f < 0) or (a_f < 0 and b_f > 0):
         # print("a & b", a, b)
         if round(a_f, precision) == round(b_f, precision):
-            print("a & b", a, b)
+            # print("a & b", a, b)
             return a
         c = middlepoint(a, b)
         # print("c", c)
         c_f = f(c)
         if c_f == 0:
-            print("c",c)
+            # x = c ja f(c) = 0
             return c
         if (a_f > 0 and c_f < 0) or (a_f < 0 and c_f > 0):
-            # print("fac")
+            # uusi tarkasteluväli on [a, c]
             return solve(f,a, c)
         else:
-            # print("fcb")
+            # uusi tarkasteluväli on [c, b]
             return solve(f, c, b)
     else:
         return
@@ -43,8 +48,9 @@ def solve(f, a: int, b: int) -> int:
 
 x = None
 while x is None:
-    a = randint(-N, N)
-    b = randint(-N, N)
+    print("Start!")
+    # valitaan tarkistelu väli [a, b] osittain satunnaisesti niin monta kertaa kuin on pakko
+    a, b = randint(-N, N), randint(-N, N)
     x=solve(f, a, b)
-    print("RESTART")
+
 print(x)
